bed6d9b9d1d0006bdcbe8aff358e82373c793cfb
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-18 11:51:55 +0200

    learn modern concurrency

---
+++ modern-concurrency.playground/Contents.swift
@@ -0,0 +1,83 @@
+/// Copyright (c) 2022 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+let task = Task {
+    print("This is first")
+
+    let sum = (1 ... 1000000).reduce(0, +)
+    try Task.checkCancellation()
+    print("This is second: 1 + 2 + 3 + ... 100 = \(sum)")
+}
+
+print("This is last")
+task.cancel()
+print("-----")
+
+Task {
+    print("Starting the task.")
+    try await Task.sleep(nanoseconds: 1_000_000_000)
+    print("Finishing the task.")
+}
+
+print("-----")
+
+func performTask() async throws {
+    print("Starting the task.")
+    try await Task.sleep(nanoseconds: 1_000_000_000)
+    print("Finishing the task.")
+}
+
+Task {
+    try await performTask()
+}
+
+func fetchDomains() async throws -> [Domain] {
+    let url = URL(string: "https://api.kodeco.com/api/domains")!
+    let (data, _) = try await URLSession.shared.data(from: url)
+
+    return try JSONDecoder().decode(Domains.self, from: data).data
+}
+
+Task {
+    do {
+        let domains = try await fetchDomains()
+
+        for (index, domain) in domains.enumerated() {
+            let attr = domain.attributes
+            print("\(index + 1) \(attr.name): \(attr.description) - \(attr.level)")
+        }
+    } catch {
+        print(error.localizedDescription)
+    }
+}
